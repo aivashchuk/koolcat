d3eae182dde5fd07c86a1757dc1ff41ff51baacf
Andriy Ivashchuk <aivashchuk@Illias-MacBook-Pro.local>
2019-06-07 18:45:41 +0300

    remove social icons from contact page

--- wp-content/themes/koolcat/page-contact.php
+++ wp-content/themes/koolcat/page-contact.php
@@ -21,28 +21,28 @@ get_header();
         <h2>
             <?php the_field('title_form','option'); ?>
         </h2>
-        <div class="d-flex align-items-center flex-wrap social">
-            <?php if(get_field('instagram','option')): ?>
-                <a href="<?php the_field('instagram','option'); ?>" target="_blank">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/svg/instagram.svg" alt="">
-                </a>
-            <?php endif; ?>
-            <?php if(get_field('twitter','option')): ?>
-                <a href="<?php the_field('twitter','option'); ?>" target="_blank">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/svg/twitter-logo-silhouette.svg" alt="">
-                </a>
-            <?php endif; ?>
-            <?php if(get_field('facebook','option')): ?>
-                <a href="<?php the_field('facebook','option'); ?>" target="_blank">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/svg/facebook-logo.svg" alt="">
-                </a>
-            <?php endif; ?>
-            <?php if(get_field('youtube','option')): ?>
-                <a href="<?php the_field('youtube','option'); ?>" target="_blank">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/svg/youtube.svg" alt="">
-                </a>
-            <?php endif; ?>
-        </div>
+<!--        <div class="d-flex align-items-center flex-wrap social">-->
+<!--            --><?php //if(get_field('instagram','option')): ?>
+<!--                <a href="--><?php //the_field('instagram','option'); ?><!--" target="_blank">-->
+<!--                    <img src="--><?php //echo get_template_directory_uri(); ?><!--/assets/dist/svg/instagram.svg" alt="">-->
+<!--                </a>-->
+<!--            --><?php //endif; ?>
+<!--            --><?php //if(get_field('twitter','option')): ?>
+<!--                <a href="--><?php //the_field('twitter','option'); ?><!--" target="_blank">-->
+<!--                    <img src="--><?php //echo get_template_directory_uri(); ?><!--/assets/dist/svg/twitter-logo-silhouette.svg" alt="">-->
+<!--                </a>-->
+<!--            --><?php //endif; ?>
+<!--            --><?php //if(get_field('facebook','option')): ?>
+<!--                <a href="--><?php //the_field('facebook','option'); ?><!--" target="_blank">-->
+<!--                    <img src="--><?php //echo get_template_directory_uri(); ?><!--/assets/dist/svg/facebook-logo.svg" alt="">-->
+<!--                </a>-->
+<!--            --><?php //endif; ?>
+<!--            --><?php //if(get_field('youtube','option')): ?>
+<!--                <a href="--><?php //the_field('youtube','option'); ?><!--" target="_blank">-->
+<!--                    <img src="--><?php //echo get_template_directory_uri(); ?><!--/assets/dist/svg/youtube.svg" alt="">-->
+<!--                </a>-->
+<!--            --><?php //endif; ?>
+<!--        </div>-->
         <?php echo do_shortcode('[contact-form-7 id="23" title="Contact Footer"]'); ?>
         <div class="text">
             <p><?php the_field('text_form','option'); ?></p>
